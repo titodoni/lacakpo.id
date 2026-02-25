791aeff8d11eb5730e154f1c02abfdbf9ecfc220
tito doni <gitarkopong@Gmail.com>
2026-02-25 10:27:23 +0700

    Fix PrismaLibSql casing

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { createClient } from '@libsql/client';
-import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -17,7 +17,7 @@ function createPrismaClient() {
       authToken: process.env.DATABASE_AUTH_TOKEN,
     });
     
-    const adapter = new PrismaLibSQL(libsql);
+    const adapter = new PrismaLibSql(libsql);
     return new PrismaClient({ adapter });
   }
   
